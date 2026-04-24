5b982a0e3ae7471b82e0b542db513d7abc7ff328
Cristescu Ioana-Maria <ioanacristescu114@gmail.com>
2026-04-24 21:52:00 +0300

    Update mux9bit_tb.v

--- mux9bit_tb.v
+++ mux9bit_tb.v
@@ -2,14 +2,11 @@
 
 module mux9bit_tb;
 
-    // 1. Declare signals
     reg [8:0] a;
     reg [8:0] b;
     reg       sel;
     wire [8:0] out;
 
-    // 2. Instantiate the Unit Under Test (UUT)
-    // This connects to the module we designed earlier
     mux9bit uut (
         .a(a),
         .b(b),
@@ -17,29 +14,23 @@ module mux9bit_tb;
         .out(out)
     );
 
-    // 3. Stimulus generation
     initial begin
-        // Display a header in the console
         $display("Time\t sel \t a \t\t b \t\t out");
         $monitor("%0t\t %b \t %b \t %b \t %b", $time, sel, a, b, out);
 
-        // Test Case 1: Select Input A
         a = 9'b000010101; b = 9'b101100110; sel = 1'b0;
-        #10; // Wait 10 time units
+        #10;
 
-        // Test Case 2: Select Input B
         sel = 1'b1;
         #10;
 
-        // Test Case 3: Change A while sel is 1 (Output should stay as B)
         a = 9'b111000101;
         #10;
 
-        // Test Case 4: Switch back to A (Output should now be the new A)
         sel = 1'b0;
         #10;
 
-        $finish; // End the simulation
+        $finish; 
     end
 
 endmodule
